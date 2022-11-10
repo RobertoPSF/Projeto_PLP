f03d330dff80f768090acb4f3d27ab4a1e4a6ace
121210587 ROBERTO PEREIRA DOS SANTOS FILHO <roberto.santos.filho@lcc2-27.lcc.ufcg.edu.br>
2022-11-10 11:11:27 -0300

    Criando os menus

---
+++ MenuDono.hs
@@ -0,0 +1,8 @@
+menuDono:: IO()
+menuDono = do
+    putStr("\n----------Menu do Dono----------\n")
+    putStr("1 - Cadastrar funcionário\n")
+    putStr("2 - Excluir funcionário\n")
+    putStr("3 - Gerência de finanças\n")
+    putStr("4 - Visualizar funcionários ativos\n")
+    putStr("5 - Voltar para o menu principal\n")
---
+++ MenuFuncionario.hs
@@ -0,0 +1,9 @@
+menuFuncionario:: IO()
+menuFuncionario= do
+    putStr("\n---------Menu do Funcionário----------\n")
+    putStr("1 - Veículos disponíveis\n")
+    putStr("2 - Escolher veículo para um cliente\n")
+    putStr("3 - Exibir clientes cadastrados\n")
+    putStr("4 - Excluir clientes do sistema\n")
+    putStr("5 - Calcular valor do veículo\n")
+    putStr("6 - Voltar para o menu principal\n")
